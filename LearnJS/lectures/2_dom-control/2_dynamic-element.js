50aa3ce5985a158be6e0c87a66fe9f61c1dc1351
YuSoYeong <1028ragon@naver.com>
2025-05-17 17:25:15 +0900

    Delete LearnJS/lectures/2_dom-control directory

--- LearnJS/lectures/2_dom-control/2_dynamic-element.js
+++
@@ -1,9 +0,0 @@
-let addBtn = document.getElementById('button');
-let inputBox = document.querySelector('input');
-
-addBtn.addEventListener('click', function() {
-  let li = document.createElement('li');
-  let ul = document.querySelector('ul');
-  li.textContent = inputBox.value;
-  ul.appendChild(li);
-});
